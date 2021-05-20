2244555ff8577fef5e222bd26a9f65ad5d9adeec
yhHuang <qa4510qa@gmail.com>
2021-05-20 13:56:01 +0800

    feat: finish aWeekRiskAssesment
    Signed-off-by: yhHuang <qa4510qa@gmail.com>

--- main.py
+++ main.py
@@ -13,31 +13,36 @@ from datetime import datetime
 CWB_API_Key = 'CWB-B7BC29A4-FADA-4DE6-9918-F2FA71A55F80'
 AGRi_API_ProjectKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0bmFtZSI6IlNETGFiX0lvVF9Qcm9qZWN0XzIwMjEiLCJuYW1lIjoicjA4NjIyMDA1IiwiaWF0IjoxNjEzNzk5Nzk2fQ.elm1KN4D7geluaVTrv-J9OoZ9aFEOFb-juiVfUrFQTc'
 now = datetime.now()
+fuzzyRange = {"AirTC_Avg":1, "RH":5, "PAR_Avg":50, "VW_F_Avg":5, "WS_ms_Avg":3, "Water_Demand":10}
 riskScale = ["very_low","low","slightly_low","proper","slightly_high","high","very_high"]
 riskLightScale = ["red","orange","yellow","green","yellow","orange","red"]
-lightScale = ["green","yellow","orange","red"]
 
-def getHazardScale(crop, growthStage):
+def getHazardScale(crop, growthStage, timeScale):
   hazardScale = {} #{[stage]:{[climateFactor]:[value]}}
-  current_time = now.strftime("%H")
-  if int(current_time) >= 6 and int(current_time) < 18:
-    conditionItem = growthStage + "_day"
-  else:
-    conditionItem = growthStage + "_night" # get the objective condition for crop in such stage and time
+  if timeScale == "realTime":
+    current_time = now.strftime("%H")
+    if int(current_time) >= 6 and int(current_time) < 18:
+      conditionItem = growthStage + "_day"
+    else:
+      conditionItem = growthStage + "_night" # get the objective condition for crop in such stage and time
 
-  # allFileList = os.listdir('./cropClimateCondition/{0}'.format(crop))
-  # for file in allFileList:
-  #   if file == conditionItem: 
-  #     hazardScale['{0}'.format(file[:-4])] = {}
-
-  # for i in hazardScale.keys():
-  with open('./cropClimateCondition/{0}/{1}.csv'.format(crop ,conditionItem), newline='') as csvfile:
-    rows = csv.reader(csvfile)
-    for row in rows:
-      print(row)
-      if row[0] != '\ufeff':
-        hazardScale[row[0]] = row[1:] 
-  print('hazardScale: {0}'.format(hazardScale))
+    with open('./cropClimateCondition/{0}/{1}.csv'.format(crop ,conditionItem), newline='') as csvfile:
+      rows = csv.reader(csvfile)
+      for row in rows:
+        if row[0] != '\ufeff':
+          hazardScale[row[0]] = row[1:] 
+  elif timeScale == "aWeek":
+    with open('./cropClimateCondition/{0}/{1}_day.csv'.format(crop ,growthStage), newline='') as csvfile:
+      rows = csv.reader(csvfile)
+      for row in rows:
+        if row[0] != '\ufeff':
+          hazardScale['{0}_day'.format(row[0])] = row[1:]
+    with open('./cropClimateCondition/{0}/{1}_night.csv'.format(crop ,growthStage), newline='') as csvfile:
+      rows = csv.reader(csvfile)
+      for row in rows:
+        if row[0] != '\ufeff':
+          hazardScale['{0}_night'.format(row[0])] = row[1:]
+  print('{0} hazardScale: {1}\n'.format(timeScale, hazardScale))
   return hazardScale
 
 def realTimeRiskAssessment(crop, postalCode, hazardScale):
@@ -45,16 +50,16 @@ def realTimeRiskAssessment(crop, postalCode, hazardScale):
   climateData_realTime = []
   riskLight = {}
   
-  with open ('./indoorClimateData/indoorClimateData_scheme.txt', 'r') as indoorDataScheme:
+  with open ('./realTime_IoT_data/indoorClimateData/indoorClimateData_scheme.txt', 'r') as indoorDataScheme:
     dataLabel_indoor = indoorDataScheme.readline().split(',')
 
-  with open ('./outdoorClimateData/outdoorClimateData_scheme.txt', 'r') as outdoorDataScheme:
+  with open ('./realTime_IoT_data/outdoorClimateData/outdoorClimateData_scheme.txt', 'r') as outdoorDataScheme:
     dataLabel_outdoor = outdoorDataScheme.readline().split(',')
 
-  with open ('./systemCapacityData/systemCapacityData_scheme.txt', 'r') as systemDataScheme:
+  with open ('./realTime_IoT_data/systemCapacityData/systemCapacityData_scheme.txt', 'r') as systemDataScheme:
     dataLabel_system = systemDataScheme.readline().split(',')
   
-  with open('./indoorClimateData/testdata_indoor.txt', 'rb') as indoorData:
+  with open('./realTime_IoT_data/indoorClimateData/testdata_indoor.txt', 'rb') as indoorData:
     line_indoor = indoorData.readline()
     off = -120
     while True:
@@ -65,7 +70,7 @@ def realTimeRiskAssessment(crop, postalCode, hazardScale):
         break
       off*=2 
 
-  with open('./outdoorClimateData/testdata_outdoor.txt', 'rb') as outdoorData:
+  with open('./realTime_IoT_data/outdoorClimateData/testdata_outdoor.txt', 'rb') as outdoorData:
     line_outdoor = outdoorData.readline()
     off = -120
     while True:
@@ -85,20 +90,32 @@ def realTimeRiskAssessment(crop, postalCode, hazardScale):
       print('{0}: {1}'.format(i, value))
     print(hazardScale[i])
     for j in range(6):
-      print(hazardScale[i][j])
+      # print(hazardScale[i][j])
       if hazardScale[i][j]== '-':
         continue
-      elif j==0 and int(hazardScale[i][j])>=float(value):
+      elif j==0 and int(hazardScale[i][j])-fuzzyRange[i]>=float(value):
         risk[i]=riskScale[0]
         break
-      elif j==5:
+      elif j==5 and int(hazardScale[i][j])+fuzzyRange[i]<=float(value):
         risk[i]=riskScale[6]
-      elif int(hazardScale[i][j])<=float(value) and int(hazardScale[i][j+1])>=float(value):
+      elif (int(hazardScale[i][j])+fuzzyRange[i])<=float(value) and (int(hazardScale[i][j+1])-fuzzyRange[i])>=float(value):
         risk[i]=riskScale[j+1]
         break
-  print(risk)
+      elif float(value)>=(int(hazardScale[i][j])-fuzzyRange[i]) and float(value)<=(int(hazardScale[i][j])+fuzzyRange[i]):
+        r=random.random()
+        print('r: {0}'.format(r))
+        if r>=(float(value)-(int(hazardScale[i][j])-fuzzyRange[i]))/(2*fuzzyRange[i]):
+          risk[i]=riskScale[j]
+        else:
+          risk[i]=riskScale[j+1]
+        print('{0}: {1}'.format(i,risk[i]))
+        break
+  print('\nrealTime riskScale:{0}'.format(risk))
   riskLight={}
-  riskLight["強風"]=riskLightScale[riskScale.index(risk["WS_ms_Avg"])]
+  if risk["WS_ms_Avg"][-3:]=="low":
+    riskLight['強風']="green"
+  else:
+    riskLight["強風"]=riskLightScale[riskScale.index(risk["WS_ms_Avg"])]
   if risk["RH"][-3:]=="low":
     riskLight['高濕']="green"
   else:
@@ -116,17 +133,19 @@ def realTimeRiskAssessment(crop, postalCode, hazardScale):
     riskLight["低溫"]="green"
     riskLight['高溫']=riskLightScale[riskScale.index(risk["AirTC_Avg"])]
   
-  print(riskLight)
+  print('realTime riskLight:{0}'.format(riskLight))
   return riskLight
 
-def aWeekRiskAssessment(crop, postalCode, growthStage):
-  climatData_aWeekForecast = {}
-
+def aWeekRiskAssessment(crop, postalCode, growthStage, hazardScale):
+  rawData = {"MinT":[],"MaxT":[],"RH":[],"WS":[]}
+  riskLight={"高溫":[],"低溫":[],"高濕":[],"強風":[]}
   try:
-    #climatData_aWeekForecast = requests.get('https://agriapi.tari.gov.tw/api/CWB_ObsWeathers/observation?postalCode={0}&hours=1&projectkey={1}'.format(postalCode ,AGRi_API_ProjectKey))
-    climatData_aWeekForecast = requests.get('https://agriapi.tari.gov.tw/api/CWB_PreWeathers/oneweekPredictions?postalCode={0}&eleName=T&projectkey={1}'.format(postalCode ,AGRi_API_ProjectKey))
-    climatData_aWeekForecast = climatData_aWeekForecast.json()
-    print(climatData_aWeekForecast)
+    climatData_aWeekForecast = requests.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/{0}?locationName={1}&elementName={2}&sort={3}&Authorization={4}'.format("F-D0047-007","中壢區","","time",CWB_API_Key))
+    #climatData_aWeekForecast = requests.get('https://agriapi.tari.gov.tw/api/CWB_PreWeathers/oneweekPredictions?postalCode={0}&eleName=MinT%2CMaxT%2CRH%2CWind&projectkey={1}'.format(postalCode ,AGRi_API_ProjectKey))
+    climatData_aWeekForecast = climatData_aWeekForecast.json()["records"]["locations"][0]["location"][0]["weatherElement"] #["output"]["data"]["content"]
+    climatData_aWeekForecast=list(filter(lambda x: x['elementName']=="RH" or x['elementName']=="WS" or x['elementName']=="MaxT" or x['elementName']=="MinT", climatData_aWeekForecast))
+    # print(len(climatData_aWeekForecast))
+
   except requests.exceptions.HTTPError as errh:
       print(errh)
   except requests.exceptions.ConnectionError as errc:
@@ -135,6 +154,87 @@ def aWeekRiskAssessment(crop, postalCode, growthStage):
       print(errt)
   except requests.exceptions.RequestException as err:
       print(err)
+  
+  for i in climatData_aWeekForecast:
+    name=i["elementName"]
+    now_day = now.strftime("%m-%d")
+    data=list(filter(lambda x: x['startTime'][5:10]!=now_day , i["time"]))
+    for j in data:
+      rawData[name].append(float(j["elementValue"][0]["value"]))
+  print (rawData)
+  
+  indoorData = indoorClimateDataEstimation("aWeek",rawData)
+  print(indoorData)
+
+  risk = {"MinT":[],"MaxT":[],"RH":[],"WS":[]}
+  for i in indoorData:
+    for j in range(len(indoorData[i])):
+      value = indoorData[i][j]
+      corrFactor = {"MinT":"AirTC_Avg","MaxT":"AirTC_Avg","RH":"RH","WS":"WS_ms_Avg"} #climate factor and risk factor corresponding
+      hazardScaleItem = []
+      print(float(value))
+      if j%2 == 0: #day(6:00 ~ 18:00)
+        if i == 'MinT' or i == 'MaxT':
+          hazardScaleItem = hazardScale["AirTC_Avg_day"]
+        elif i == "RH":
+          hazardScaleItem = hazardScale["RH_day"]
+        elif i == "WS":
+          hazardScaleItem = hazardScale["WS_ms_Avg_day"]
+      else: #night(18:00 ~ 6:00)
+        if i == 'MinT' or i == 'MaxT':
+          hazardScaleItem = hazardScale["AirTC_Avg_night"]
+        elif i == "RH":
+          hazardScaleItem = hazardScale["RH_night"]
+        elif i == "WS":
+          hazardScaleItem = hazardScale["WS_ms_Avg_night"]
+      print(hazardScaleItem)
+      for k in range(6):
+        if hazardScaleItem[k]== '-':
+          continue
+        elif k==0 and int(hazardScaleItem[k])-fuzzyRange[corrFactor[i]]>=float(value):
+          risk[i].append(riskScale[0])
+          break
+        elif k==5 and int(hazardScaleItem[k])+fuzzyRange[corrFactor[i]]<=float(value):
+          risk[i].append(riskScale[6])
+        elif (int(hazardScaleItem[k])+fuzzyRange[corrFactor[i]])<=float(value) and (int(hazardScaleItem[k+1])-fuzzyRange[corrFactor[i]])>=float(value):
+          risk[i].append(riskScale[k+1])
+          break
+        elif float(value)>=(int(hazardScaleItem[k])-fuzzyRange[corrFactor[i]]) and float(value)<=(int(hazardScaleItem[k])+fuzzyRange[corrFactor[i]]):
+          r=random.random()
+          print('r: {0}'.format(r))
+          if r>=(float(value)-(int(hazardScaleItem[k])-fuzzyRange[corrFactor[i]]))/(2*fuzzyRange[corrFactor[i]]):
+            risk[i].append(riskScale[k])
+          else:
+            risk[i].append(riskScale[k+1])
+          break
+      print('{0}: {1}'.format(i,risk[i]))
+  print('\naWeek riskScale:{0}'.format(risk))
+
+  for i in risk:
+    for j in range(len(risk[i])):
+      if i == "MinT":
+        if risk[i][j][-3:]=="igh":
+          riskLight["低溫"].append("green")
+        else:
+          riskLight["低溫"].append(riskLightScale[riskScale.index(risk[i][j])])
+      elif i == "MaxT":
+        if risk[i][j][-3:]=="low":
+          riskLight["高溫"].append("green")
+        else:
+          riskLight["高溫"].append(riskLightScale[riskScale.index(risk[i][j])])
+      elif i == "RH":
+        if risk[i][j][-3:]=="low":
+          riskLight["高濕"].append("green")
+        else:
+          riskLight["高濕"].append(riskLightScale[riskScale.index(risk[i][j])])
+      elif i == "WS":
+        if risk[i][j][-3:]=="low":
+          riskLight["強風"].append("green")
+        else:
+          riskLight["強風"].append(riskLightScale[riskScale.index(risk[i][j])])
+  
+  print('aWeek riskLight:{0}'.format(riskLight))
+  return riskLight
 
 # def seasonalLongTermRiskAssessment(crop, timeRange):
 
@@ -144,17 +244,19 @@ def operationAssessment(timeScale,riskLight):
   operationalSuggestion={}
   operationData = open ('./operationMethod/operationMethod_{0}.csv'.format(timeScale), 'r')
   for line in operationData.readlines():
-    print(line)
     line=line.split(',')
     if line[0] in riskLight.keys():
-      print(line[-1])
-      print(riskLight[line[0]])
       if riskLight[line[0]] == line[-1].replace("\n",""):
         operationalSuggestion[line[0]]={
           "method":line[1],
           "risk_factor":line[2]
         }
+  print('{0} operationalSuggestion:{1}\n'.format(timeScale, operationalSuggestion))
   return operationalSuggestion
+
+def indoorClimateDataEstimation(timescale,data):
+  return data
+
 
 def main():
   operationalSuggestion={}
@@ -162,10 +264,13 @@ def main():
     postalCode = inputData.readline().split(':')[1].replace("\n", "")
     crop = inputData.readline().split(':')[1].replace("\n", "")
     growthStage = inputData.readline().split(':')[1].replace("\n", "")
-  hazardScale = getHazardScale(crop, growthStage)
-  riskLight_realTime = realTimeRiskAssessment(crop, postalCode, hazardScale)
+  realTimehazardScale = getHazardScale(crop, growthStage, "realTime")
+  riskLight_realTime = realTimeRiskAssessment(crop, postalCode, realTimehazardScale)
   operationalSuggestion["realTime"]=operationAssessment("realTime",riskLight_realTime)
-  aWeekRiskAssessment(crop, postalCode, growthStage)
+  aWeekhazardScale = getHazardScale(crop, growthStage, "aWeek")
+  riskLight_aWeek = aWeekRiskAssessment(crop, postalCode, growthStage, aWeekhazardScale)
+  operationalSuggestion["aWeek"]=operationAssessment("aWeek",riskLight_aWeek)
+
 
   # crop = sys.argv[1] #[cropItem]
   # timeScale = sys.argv[2] #'realTimeRiskAssessment',aWeekRiskAssessment ,'seasonalLongTermRiskAssessment', 'climateChangeRiskAssessment'
